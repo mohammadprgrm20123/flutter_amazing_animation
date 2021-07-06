54f22b7c9c286ab38f5fb022ce0d7c2ccc1a923e
mohammad kazemi <kazeminejad95.itsu@yahoo.com>
2021-07-06 21:32:47 +0430

    add licnese

---
+++ lib/flutter_module_animation.dart
@@ -0,0 +1,15 @@
+library flutter_module_aniamtion;
+
+export 'package:flutter_module_animation/aniamtion/fade/fade_in_left.dart';
+export 'package:flutter_module_animation/aniamtion/fade/fade_in_down.dart';
+export 'package:flutter_module_animation/aniamtion/fade/fade_in_right.dart';
+export 'package:flutter_module_animation/aniamtion/fade/fade_in_up.dart';
+export 'package:flutter_module_animation/aniamtion/bounce/bounce_right.dart';
+export 'package:flutter_module_animation/aniamtion/bounce/bounce_left.dart';
+export 'package:flutter_module_animation/aniamtion/bounce/bounce_out.dart';
+export 'package:flutter_module_animation/aniamtion/bounce/bounce_in.dart';
+export 'package:flutter_module_animation/aniamtion/zoom/zoom.dart';
+export 'package:flutter_module_animation/aniamtion/elastic/elastic_left.dart';
+export 'package:flutter_module_animation/aniamtion/elastic/elastic_out.dart';
+export 'package:flutter_module_animation/aniamtion/elastic/elastic_right.dart';
+export 'package:flutter_module_animation/aniamtion/elastic/elasticIn.dart';
